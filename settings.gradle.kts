68c8f31e09409a48aa3c94df16a6f8b80f59daf4
Tavon <kamklock@gmail.com>
2026-09-22 19:09:09 -0600

    Fix GeckoView repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories { google(); mavenCentral(); maven { url = uri("https://maven.mozilla.org/maven2") } }
 }
 rootProject.name = "YtubemediaTV"
 include(":app")
